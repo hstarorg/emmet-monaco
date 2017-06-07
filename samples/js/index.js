b108c7e127e72d0912991562627c862b48b1c798
Jay.M.Hu <jay.m.hu@newegg.com>
2017-06-07 18:43:08 +0800

    emmet support.

--- samples/js/index.js
+++ samples/js/index.js
@@ -1,7 +1,7 @@
 require.config({ paths: { 'vs': 'vendor/monaco-editor/dev/vs' } });
 require(['vs/editor/editor.main'], function () {
   var editor1 = monaco.editor.create(document.getElementById('editor1'), {
-    value: '',
+    value: 'ul#nav>li.item$*4>a{Item $}',
     language: 'html'
   });
   window['emmet-monaco'].enableEmmet(editor1, window.emmet);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const expandAbbreviation = (emmet, source, language) => {
+  let target = emmet.expandAbbreviation(source, language, language);
+  let result = emmet.tabStops.extract(target, { escape(ch) { return ch; } });
+  return result.text;
+};
+
 const enableEmmet = (editor, emmet, options) => {
   if (!emmet) {
     throw new Error('Must include emmet.');
@@ -5,7 +11,23 @@ const enableEmmet = (editor, emmet, options) => {
   if (!editor) {
     throw new Error('Must provide monaco-editor instance.');
   }
-
+  editor.addCommand(monaco.KeyCode.Tab, () => {
+    let word = editor.model.getValueInRange(editor.getSelection());
+    let pos = editor.getPosition();
+    if (!word) {
+      let lineContent = editor.model.getLineContent(pos.lineNumber);
+      word = emmet.utils.action.extractAbbreviation(lineContent.substring(0, pos.column));
+    }
+    // Get expand text
+    let expandText = expandAbbreviation(emmet, word, 'html');
+    if (expandText) {
+      // replace range content: pos.column , pos.column -word.length;
+      let range = new monaco.Range(pos.lineNumber, pos.column - word.length, pos.lineNumber, pos.column);
+      let id = { major: 1, minor: 1 };
+      var op = { identifier: id, range, text: expandText, forceMoveMarkers: true };
+      editor.executeEdits('', [op]);
+    }
+  });
 };
 
 export {
